07ecb855814c76cf09fdd50b363e795a8c54abde
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-01-13 16:30:55 +0900

    DFS/10026/적록색약 - 풀이, 수정
    불필요한 변수 삭제, for문 수정

--- 백준/DFS와 BFS/[10026]적록색약/main.cpp
+++ 백준/DFS와 BFS/[10026]적록색약/main.cpp
@@ -8,29 +8,28 @@ enum RGB{
 	r,
 	g,
 	b
-};
-
-int map[MAXN][MAXN];
-bool visited[MAXN][MAXN];
-int n;
-int RGBcnt[3]; // 빨강, 초록, 파랑 그룹 개수 
-int dr[4] = {0,0,-1,1};
-int dc[4] = {1,-1,0,0};
+}; 
 
 RGB R(r);
 RGB G(g);
 RGB B(b);
+
+int map[MAXN][MAXN];
+bool visited[MAXN][MAXN];
+int n;
+int dr[4] = {0,0,-1,1};
+int dc[4] = {1,-1,0,0};
+
 
 using namespace std;
 
 
 void DFS(int i, int j, int color){
 	visited[i][j] = true;
-	int row, col;
 	for(int k = 0; k < 4; k++){
-		row = i + dr[k];
-		col = j + dc[k];
-		if(row < 0 || row >= n || col < 0 || col >=n) continue;
+		int row = i + dr[k];
+		int col = j + dc[k];
+		if(row <= 0 || row > n || col <= 0 || col > n) continue;
 		else if(!visited[row][col] && map[row][col] == color){
 			DFS(row, col, color);
 		}
@@ -44,40 +43,57 @@ int main(){
 	cin >> n;
 	
 	// 초기화
-	for(int i = 0; i < n; i++){
+	for(int i = 1; i <= n; i++){
 		for(int j = 1; j <= n; j++){
 			map[i][j] = -1;
 			visited[i][j] = false;
 		}
 	} 
-	fill_n(RGBcnt, 3, 0);
 
 	// 입력 
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
 			char tmp;
 			cin >> tmp;
 			if(tmp == 'R') map[i][j] = R;
-			else if (tmp == 'G') map[i][j] = G;
+			else if(tmp == 'G') map[i][j] = G;
 			else if (tmp == 'B') map[i][j] = B;
 		}
 	}
 
+	int normal = 0;
 	// map의 각 칸마다 조건 확인 후, DFS 호출
-	for(int i = 0; i < n; i++){
-		for(int j = 0 ; j < n; j++){
-			int color = map[i][j];
+	for(int i = 1; i <= n; i++){
+		for(int j = 1 ; j <= n; j++){
 			if(!visited[i][j]){
-				RGBcnt[color]++;
+				int color = map[i][j];
 				DFS(i,j,color);
+				normal++;
+			}
+		}
+	} 
+
+
+	// 초기화, R을 G로 바꾸기 
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			visited[i][j] = false;
+			if(map[i][j] == R) map[i][j]  = G;
+		}
+	}
+	
+	int wrong = 0;
+	// map의 각 칸마다 조건 확인 후, DFS 호출
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= n; j++){
+			if(!visited[i][j]){
+				int color = map[i][j];
+				DFS(i,j, color);
+				wrong++;
 			}
 		}
 	} 
 	
-	// 결과 출력
-	int normal = RGBcnt[0] + RGBcnt[1] + RGBcnt[2];
-	int wrong = RGBcnt[0] > RGBcnt[1] ? RGBcnt[0] + RGBcnt[2] : RGBcnt[1] + RGBcnt[2];
-
 	cout << normal << " " << wrong << endl;
 		
 	return 0;
