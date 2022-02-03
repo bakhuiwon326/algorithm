8b485d82fb44a9096374fc3e0e6f97c0d404ea35
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-02-03 14:22:01 +0900

    DP/1463/1로만들기 - 풀이

---
+++ 백준/알고리즘별/DP/[1463]1로만들기/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int n;
+int d[1000001];
+
+
+int main(){
+	
+	scanf("%d", &n);
+	
+	d[1] = 0;
+	
+	for(int i = 2 ; i <= n; i++){
+		d[i] = d[i-1] + 1;
+		if(i % 3 == 0) d[i] = min(d[i], d[i/3] + 1);
+		if( i % 2 == 0) d[i] = min(d[i], d[i/2] + 1);
+	}
+	
+	printf("%d", d[n]);
+	
+	return 0;
+}
