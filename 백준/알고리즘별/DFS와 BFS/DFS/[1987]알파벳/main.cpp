5924acf51b5433df88b4f12a440dcd9a562744b6
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-07-12 03:17:52 +0900

    DFS/1987/알파벳 - 풀이
    풀이 성공!

--- 백준/알고리즘별/DFS와 BFS/DFS/[1987]알파벳/main.cpp
+++ 백준/알고리즘별/DFS와 BFS/DFS/[1987]알파벳/main.cpp
@@ -1,71 +1,54 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
+#include<vector>
 
-#define MAXN 21
+#define MAXR 21
 
 using namespace std;
 
-int r,c,cnt;
-char map[MAXN][MAXN];
-bool visited[MAXN][MAXN];
-bool alphabet[26];
-int dr[4] = {0,0,1,-1};
-int dc[4] = {1,-1,0,0};
+int r, c, cnt;
+int maxCnt = -1;
+char map[MAXR][MAXR];
+int passedAlphabet[26];
+int dr[4] = { 0,0,-1,1 };
+int dc[4] = { 1,-1,0,0 };
+vector<char> path;
 
-void init(){
-	for(int i = 0 ; i < r; i++){
-		for(int j = 0 ; j < c ; j++){
-			map[i][j] = 'A';
-			visited[i][j] = false;
+void DFS(int row, int col, int count) {
+
+	char alphabet = map[row][col];
+	if (passedAlphabet[alphabet - 'A'] >= 1) return;
+	else passedAlphabet[alphabet - 'A']++;
+	count++;
+
+	if (maxCnt < count) maxCnt = count;
+
+	for (int i = 0; i < 4; i++) {
+		int x = row + dr[i];
+		int y = col + dc[i];
+		if (x >= 1 && x <= r && y >= 1 && y <= c) {
+			if (passedAlphabet[map[x][y] - 'A'] == 0) {
+				DFS(x, y, count);
+				passedAlphabet[map[x][y] - 'A'] = 0;
+			}
 		}
-	}	
-	for(int i = 0; i < 26; i++) alphabet[i] = false;
-	cnt = 0;
+	}
 }
 
-void DFS(int i, int j){
-	for(int p = 0; p < 4; p++){
-		int row = i + dr[p];
-		int col = j + dc[p];
-		if(row < 0 || row >= r || col < 0 || col >= c) continue;
-		int index = map[row][col] - 'A';
-		if(!alphabet[index] && !visited[row][col]){
-			visited[row][col] = true;
-			alphabet[index] = true;
-			cnt++;
-			DFS(row, col);
-		}
-		
-	}
+int main() {
 	
-}
+	scanf("%d %d", &r, &c);
 
-
-int main(){
-	
-	std::ios::sync_with_stdio(false);
-	
-	cin >> r >> c;
-	
-	for(int i = 0 ; i < r; i++){
-		for(int j = 0 ; j < c ; j++){
-			cin >> map[i][j];
+	for (int i = 1; i <= r; i++) {
+		for (int j = 1; j <= c; j++) {
+			scanf("%1s", &map[i][j]);
 		}
 	}
-	
-	visited[0][0] = true;
-	int index = map[0][0] - 'A';
-	alphabet[index] = true;
-	cnt++;
-	DFS(0,0);
-	
-    int cnt = 0;
-	for(int i = 0 ; i < 26; i++){
-		if(alphabet[i]) cnt++;
-	}
-    
-	cout << cnt;	
-    
+
+	DFS(1, 1, 0);
+
+	printf("%d", maxCnt);
+
 	return 0;
 }
+
