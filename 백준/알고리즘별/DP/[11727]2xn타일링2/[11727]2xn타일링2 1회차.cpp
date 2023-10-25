025828976abf5e8bd623ebdd780218cf1ae7c1bc
unknown <powerup326@naver.com>
2023-10-25 11:23:15 +0900

    2xn타일링2 1회차풀이

---
+++ 백준/알고리즘별/DP/[11727]2xn타일링2/[11727]2xn타일링2 1회차.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory.h>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int main() {
+
+	memset(dp, 0, sizeof(dp));
+	scanf("%d", &n);
+
+	dp[1] = 1;
+	dp[2] = 3;
+	
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+	}
+
+	printf("%d", dp[n]);
+
+	return 0;
+}
